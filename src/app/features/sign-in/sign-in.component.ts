9aa509ff9cc858986c6bd2599739f1040149497c
Maycol Huamani <maycol.huamani.ext@expertiatravel.com>
2025-05-04 21:47:43 -0500

    feat: implement sign-in component with form and styling; update auth layout and styles

--- src/app/features/sign-in/sign-in.component.ts
+++ src/app/features/sign-in/sign-in.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputOtp } from 'primeng/inputotp';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [CardModule, ButtonModule, InputTextModule, InputOtp],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
